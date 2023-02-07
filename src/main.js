417ca692a9bce0f14dc745eef7fc2dcd32a51f86
rsensenig <r.a.sensenig@gmail.com>
2023-02-07 15:32:26 -0500

    replaced main.css with sass styling, brought over from vue3-basic-product-cart-demo project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/styles/main.css'
+// import './assets/styles/main.css'
+import './assets/styles/style.scss'
 
 const app = createApp(App)
 app.use(router)
