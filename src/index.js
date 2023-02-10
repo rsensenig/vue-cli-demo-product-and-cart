ba04993476ea7d634a79a70a5bac1de5717af64b
rsensenig <r.a.sensenig@gmail.com>
2023-02-09 20:15:45 -0500

    created custom log format

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const logger = require('./logger');
 logger.info('text info');
 logger.warn('text warn');
 logger.error('text error');
+// logger.debug('text debug');
 
 // console.log('hello');
 // console.info('text info');
--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,24 @@
-const winston = require('winston');
+const { format, createLogger, transports } = require('winston');
+const { timestamp, combine, printf } = format;
 
-const logger = winston.createLogger({
-  format: winston.format.simple(),
+// custom format:
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+// creating logger here:
+const logger = createLogger({
+  // default log level is set to info
+  // level: 'debug',
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    logFormat
+  ),
+  // format: winston.format.simple(),
 //   defaultMeta: { service: 'user-service' },
-  transports: [new winston.transports.Console()],
+  transports: [new transports.Console()],
 });
 
 module.exports = logger;
