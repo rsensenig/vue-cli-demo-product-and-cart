5c025df6aa8013f2e682ab56761c8cd7e6525909
rsensenig <r.a.sensenig@gmail.com>
2023-02-09 20:37:28 -0500

    cleaned up code on logger.js with destructuring

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
 const { format, createLogger, transports } = require('winston');
-const { timestamp, combine, printf } = format;
+const { timestamp, combine, printf, colorize, errors } = format;
 
 // custom format:
 const logFormat = printf(({ level, message, label, timestamp, stack }) => {
@@ -11,12 +11,12 @@ const logger = createLogger({
   // default log level is set to info
   // level: 'debug',
   format: combine(
-    format.colorize(),
+    colorize(),
     timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
     // if I get an error, print the stack trace
-    format.errors({stack: true}),
+    errors({stack: true}),
     logFormat
   ),
   // format: winston.format.simple(),
